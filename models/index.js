59e514ef71e6ba865f6509609bfdabd8103572ee
Yamini Yadav <ykmba9@gmail.com>
2018-09-24 00:19:29 -0700

    updated

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // Exporting an object containing all models
 
 module.exports = {
-    Articles: require("./Articles.js"),
-    Notes: require("./Comments.js")
+    Article: require("./Articles"),
+    Note: require("./Note")
   };
