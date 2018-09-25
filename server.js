f0b393456e3ce38c5b262d4dd5d5fff22043890a
Yamini Yadav <ykmba9@gmail.com>
2018-09-24 18:19:38 -0700

    added gitignore

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
